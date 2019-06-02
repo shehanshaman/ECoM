22bddaaf2cda388821ab760d46453710e9c0761f
shehan shaman <33275058+shehanshaman@users.noreply.github.com>
2019-06-02 09:53:31 +0530

    Added index.php
    index.php for "Sending data to server (000webhost) using GSM with AT" video on youtube

---
+++ ref/index.php
@@ -0,0 +1,28 @@
+<?php require_once('connection/connection.php'); ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title></title>
+ </head>
+  <body>
+
+<?php
+
+	if (isset($_GET['id']) && isset($_GET['value'])) {
+		$id = mysqli_real_escape_string($connection,$_GET['id']);
+		$value = mysqli_real_escape_string($connection,$_GET['value']);
+		$query = "insert into data values ({$id},{$value})";
+
+		$result_set = mysqli_query($connection,$query);
+		
+		if($result_set) echo "succeess : add details";
+		else echo "error on adding details"; 
+	}else echo "Access denid";	
+
+?>
+
+</body>
+</html>
+
+<?php mysqli_close($connection); ?>
